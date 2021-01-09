06e60db4d4d8a721868a1bfe7cf2b257a72a669d
胡剑峰2020 <952405831@qq.com>
2021-01-09 17:41:21 +0800

    gateway yml配置及负载均衡

---
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/GateWayMain9527.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Hujf
+ * @title: GateWayMain9527
+ * @date 2021/1/9 0009下午 5:08
+ * @description: TODO
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class,args);
+    }
+}
