e7eda03df6699681dfbcaed21b412215c1349b06
胡剑峰2020 <952405831@qq.com>
2021-01-09 11:09:18 +0800

    hystrix服务降级

--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/impl/PaymentServiceImpl.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/impl/PaymentServiceImpl.java
@@ -2,6 +2,8 @@ package com.atguigu.springcloud.service.impl;
 
 import ch.qos.logback.core.util.TimeUtil;
 import com.atguigu.springcloud.service.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -19,13 +21,22 @@ public class PaymentServiceImpl implements PaymentService {
     public String paymentInfo_OK(Integer id){
         return "线程"+Thread.currentThread().getName()+" id ="+id+" ok";
     }
+
     @Override
+    @HystrixCommand(fallbackMethod = "paymentInfo_errorHandler",commandProperties = {
+        @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "3000")
+    })
     public String paymentInfo_error(Integer id){
         try {
-            TimeUnit.SECONDS.sleep(3);
+            TimeUnit.SECONDS.sleep(2);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         return "线程"+Thread.currentThread().getName()+" id ="+id+" 耗时3S";
+    }
+
+    //服务降级实现类
+    public String paymentInfo_errorHandler(Integer id){
+        return "错误页面";
     }
 }
