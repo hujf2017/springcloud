a1bc659e07ddaa038b7d56a2f8074b6cac88bb1e
胡剑峰2020 <952405831@qq.com>
2021-01-09 15:53:57 +0800

    hystrix dashboard监控图形化

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/atguigu/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author Hujf
+ * @title: HystrixDashboardMain9001
+ * @date 2021/1/9 0009下午 3:31
+ * @description: TODO
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class,args);
+    }
+
+}
